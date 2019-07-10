651d7c8cbd5dd0ffafef939a0d744468d27a9108
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-10 09:40:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@
 
 function findMatching(drivers,name){
   return drivers.filter(function(driver){
-  return (driver.toLowerCase() === name.toLowerCase()) ;
+  return (driver.toLowerCase() === name.toLowerCase());
 });
 }
 
